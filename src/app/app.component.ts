6b5a542359324c27a2e5e02781566818d29c1852
unknown <hywang39@gmail.com>
2017-12-20 05:45:19 -0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './authentication.service'
+import { Global } from './global';
+import { UserService } from './user.service';
+
+import { LoginComponent } from './login/login.component';
+import { SummaryComponent } from './summary/summary.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+
   title = 'app';
+  users:any;
+  username: string;
+
+  constructor(private authenticationService: AuthenticationService,
+    private global: Global,
+    private userService: UserService) { }
+
+  logout() {
+    this.authenticationService.logout();
+    this.authenticationService.token = null;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule }    from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SummaryComponent } from './summary/summary.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component'
+
+import { AppRoutingModule } from './app.routing.module';
+import { AuthenticationService } from './authentication.service';
+import { HttpClientModule } from '@angular/common/http'; 
+import { HttpModule } from '@angular/http';
+import { AuthGuard } from './auth.guard';
+import { Global } from './global';
+import { UserService } from './user.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SummaryComponent,
+    LoginComponent,
+    LogoutComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpModule,
   ],
-  providers: [],
+  providers: [AuthenticationService, AuthGuard, Global, UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
